b89cf07d7bdfa9f651bfd76e4b102b3533ab7b15
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-09-11 23:28:53 +0800

    Screens: Update Tickets Page Mobile

--- lib/screens/mobile/tickets_mobile.dart
+++ lib/screens/mobile/tickets_mobile.dart
@@ -139,16 +139,34 @@ class _TicketsMobileScreenState extends State<TicketsMobileScreen> {
                   ),
                 ],
               ),
-              child: const Row(
+              child: Row(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    "Tickets Issued Mobile",
-                    style: TextStyle(
-                      color: blackColor,
-                      fontSize: 20,
-                      fontWeight: FontWeight.w500,
-                    ),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "Tickets Issued Desktop",
+                        style: TextStyle(
+                          color: blackColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          _modal(context);
+                        },
+                        child: const Text(
+                          "Modal",
+                          style: TextStyle(
+                            color: blueColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
@@ -159,6 +177,183 @@ class _TicketsMobileScreenState extends State<TicketsMobileScreen> {
           ),
         ],
       ),
+    );
+  }
+
+  void _modal(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: whiteColor,
+          scrollable: true,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          title: Container(
+            padding: const EdgeInsets.only(bottom: 12),
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(width: 0.2, color: blackColor),
+              ),
+            ),
+            child: const Text(
+              "Ticket Information",
+              style: TextStyle(
+                color: blackColor,
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          content: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Wrap(
+                    children: [
+                      Text(
+                        'Ticketed To: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        'NDA-1234 (202100228)',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    '6/28/2024',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Wrap(
+                    children: [
+                      Text(
+                        'Phone Number: ',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        '09284655371',
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    'two-wheels',
+                    style: TextStyle(
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Carrying or loading the car of any material when its edge portion causes damage or scrape the pavement of the road/street.',
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                'Attachment/s:',
+                style: TextStyle(
+                  fontWeight: FontWeight.w500,
+                  color: Colors.grey,
+                ),
+              ),
+              const SizedBox(height: 8),
+              Column(
+                children: [
+                  Container(
+                    height: 160,
+                    // width: 215,
+                    decoration: BoxDecoration(
+                      color: blueColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    height: 160,
+                    // width: 215,
+                    decoration: BoxDecoration(
+                      color: blueColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  Container(
+                    height: 160,
+                    // width: 215,
+                    decoration: BoxDecoration(
+                      color: blueColor.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                "Close",
+                style: TextStyle(
+                  color: blueColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+            TextButton(
+              style: TextButton.styleFrom(
+                backgroundColor: blueColor,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text(
+                "Resolve",
+                style: TextStyle(
+                  color: whiteColor,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ],
+        );
+      },
     );
   }
 }
