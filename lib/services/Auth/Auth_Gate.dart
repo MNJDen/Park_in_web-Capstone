a06eb89f5096cc132907e9165b21d175c29dcd9e
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-09-21 18:50:13 +0800

    Services: Add Auth

---
+++ lib/services/Auth/Auth_Gate.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:park_in_web/screens/report_main.dart';
+import 'package:park_in_web/screens/sign_in_main.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthGate extends StatelessWidget {
+  const AuthGate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const ReportMain();
+          } else {
+            return const SignInMain();
+          }
+        },
+      ),
+    );
+  }
+}
---
+++ lib/services/Auth/Auth_Service.dart
@@ -0,0 +1,76 @@
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthService {
+  // Instance of auth & firestore
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // Get current user
+  User? getCurrentUser() {
+    return _auth.currentUser;
+  }
+
+  // Error Messages
+  String getErrorMessage(String errorCode) {
+    switch (errorCode) {
+      case 'invalid-email':
+        return 'The email address is not valid.';
+      case 'user-disabled':
+        return 'This user has been disabled.';
+      case 'user-not-found':
+        return 'No user found with this email.';
+      case 'wrong-password':
+        return 'Incorrect password. Please try again.';
+      case 'email-already-in-use':
+        return 'The email address is already in use.';
+      case 'operation-not-allowed':
+        return 'Email/password accounts are not enabled.';
+      case 'weak-password':
+        return 'The password is too weak.';
+      default:
+        return 'An unknown error occurred. Please try again later.';
+    }
+  }
+
+  // Sign In
+  Future<UserCredential?> signInWithEmailPassword(
+      String email, String password) async {
+    try {
+      UserCredential userCredential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      // Retrieve user data from Firestore
+      DocumentSnapshot userDoc = await _firestore
+          .collection("Admin")
+          .doc('qaClgM9tEoe7fzdkMefZTAXgtWC3')
+          .get();
+
+      // Check if the username exists in Firestore
+      if (!userDoc.exists) {
+        throw AuthServiceException("User data not found in Firestore");
+      }
+
+      return userCredential;
+    } on FirebaseAuthException catch (e) {
+      throw AuthServiceException(getErrorMessage(e.code));
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    return await _auth.signOut();
+  }
+}
+
+class AuthServiceException implements Exception {
+  final String message;
+  AuthServiceException(this.message);
+
+  @override
+  String toString() => message; // Override toString to return just the message
+}
