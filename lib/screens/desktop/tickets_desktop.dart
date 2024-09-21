573053a9894c584de0440f45d08a74c0d350802c
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-09-21 18:50:31 +0800

    Screens: Update Backend Tickets Desktop

--- lib/screens/desktop/tickets_desktop.dart
+++ lib/screens/desktop/tickets_desktop.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:park_in_web/components/navbar/navbar_desktop.dart';
 import 'package:park_in_web/components/theme/color_scheme.dart';
+import 'package:intl/intl.dart';
 
 class TicketsDesktopScreen extends StatefulWidget {
   const TicketsDesktopScreen({super.key});
@@ -10,6 +12,39 @@ class TicketsDesktopScreen extends StatefulWidget {
 }
 
 class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
+  // Firestore instance
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  // List to hold fetched tickets
+  List<Map<String, dynamic>> tickets = [];
+  bool _isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchReports(); // Fetch data when the screen is initialized
+  }
+
+  // Fetch reports from Firestore
+  Future<void> _fetchReports() async {
+    QuerySnapshot snapshot =
+        await _firestore.collection('Violation Ticket').get();
+    setState(() {
+      tickets = snapshot.docs
+          .map((doc) => doc.data() as Map<String, dynamic>)
+          .toList();
+
+      // Sorting tickets
+      tickets.sort((a, b) {
+        DateTime dateA = (a['timestamp'] as Timestamp).toDate();
+        DateTime dateB = (b['timestamp'] as Timestamp).toDate();
+        return dateA.compareTo(dateB); // Sort in ascending order
+      });
+
+      _isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -37,37 +72,28 @@ class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
                   ),
                 ],
               ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      const Text(
-                        "Tickets Issued Desktop",
+              child: _isLoading
+                  ? const Center(child: CircularProgressIndicator())
+                  : PaginatedDataTable(
+                      header: const Text(
+                        "Tickets Issued",
                         style: TextStyle(
-                          color: blackColor,
+                          fontWeight: FontWeight.bold,
                           fontSize: 20,
-                          fontWeight: FontWeight.w500,
+                          color: blackColor,
                         ),
                       ),
-                      TextButton(
-                        onPressed: () {
-                          _modal(context);
-                        },
-                        child: const Text(
-                          "Modal",
-                          style: TextStyle(
-                            color: blueColor,
-                            fontSize: 16,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
+                      columns: const [
+                        DataColumn(label: Text("Ticket ID")),
+                        DataColumn(label: Text("Ticketed To")),
+                        DataColumn(label: Text("Vehicle Type")),
+                        DataColumn(label: Text("Violation")),
+                        DataColumn(label: Text("Status")),
+                        DataColumn(label: Text("Date")),
+                      ],
+                      source: ReportDataSource(tickets, context),
+                      rowsPerPage: 6,
+                    ),
             ),
           ),
           const SizedBox(
@@ -77,8 +103,132 @@ class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
       ),
     );
   }
+}
 
-  void _modal(BuildContext context) {
+class ReportDataSource extends DataTableSource {
+  final List<Map<String, dynamic>> tickets;
+  final BuildContext context;
+
+  ReportDataSource(this.tickets, this.context);
+
+  @override
+  DataRow getRow(int index) {
+    final ticket = tickets[index];
+    final ticketedTo = ticket['plate_number'] ?? '';
+    final vehicleType = ticket['vehicle_type'] ?? '';
+    final violation = ticket['violation'] ?? '';
+    final status = ticket['status'] ?? 'Pending';
+    final timestamp =
+        (ticket['timestamp'] as Timestamp?)?.toDate() ?? DateTime.now();
+    final imageUrl1 = ticket['close_up_image_url'] ?? '';
+    final imageUrl2 = ticket['mid_shot_image_url'] ?? '';
+    final imageUrl3 = ticket['wide_shot_image_url'] ?? '';
+
+    final formattedDate = DateFormat('MM/dd/yyyy').format(timestamp);
+    final formattedTime =
+        DateFormat('hh:mm a').format(timestamp); // 12-hour format
+    final formattedDateTime = '$formattedDate at $formattedTime';
+
+    return DataRow(cells: [
+      DataCell(Text('${index + 1}')),
+      // DataCell(Text(ticketedTo)),
+      DataCell(
+        GestureDetector(
+          onTap: () {
+            _modal(context, ticketedTo, vehicleType, violation, status,
+                formattedDateTime, imageUrl1, imageUrl2, imageUrl3);
+          },
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              ticketedTo,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+      // DataCell(Text(vehicleType)),
+      DataCell(
+        GestureDetector(
+          onTap: () {
+            _modal(context, ticketedTo, vehicleType, violation, status,
+                formattedDateTime, imageUrl1, imageUrl2, imageUrl3);
+          },
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              vehicleType,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+      // DataCell(Text(violation)),
+      DataCell(
+        GestureDetector(
+          onTap: () {
+            _modal(context, ticketedTo, vehicleType, violation, status,
+                formattedDateTime, imageUrl1, imageUrl2, imageUrl3);
+          },
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              violation,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+      // DataCell(Text(status)),
+      DataCell(
+        GestureDetector(
+          onTap: () {
+            _modal(context, ticketedTo, vehicleType, violation, status,
+                formattedDateTime, imageUrl1, imageUrl2, imageUrl3);
+          },
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              status,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+      // DataCell(Text(formattedDateTime)),
+      DataCell(
+        GestureDetector(
+          onTap: () {
+            _modal(context, ticketedTo, vehicleType, violation, status,
+                formattedDateTime, imageUrl1, imageUrl2, imageUrl3);
+          },
+          child: Container(
+            constraints: BoxConstraints(maxWidth: 250),
+            child: Text(
+              formattedDateTime,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ),
+        ),
+      ),
+    ]);
+  }
+
+  void _modal(
+      BuildContext context,
+      String ticketedTo,
+      String vehicleType,
+      String violation,
+      String status,
+      String timestamp,
+      String attachmentUrl1,
+      String attachmentUrl2,
+      String attachmentUrl3) {
     showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -111,68 +261,95 @@ class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                const Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Wrap(
-                      children: [
-                        Text(
-                          'Ticketed To: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Text(
-                          'NDA-1234 (202100228)',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Ticketed To: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
                     ),
                     Text(
-                      '6/28/2024',
+                      ticketedTo,
                       style: TextStyle(
-                        color: Colors.grey,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
                       ),
                     ),
                   ],
                 ),
                 const SizedBox(height: 16),
-                const Row(
+                Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    Wrap(
-                      children: [
-                        Text(
-                          'Phone Number: ',
-                          style: TextStyle(
-                            fontWeight: FontWeight.w500,
-                            color: Colors.grey,
-                          ),
-                        ),
-                        Text(
-                          '09284655371',
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ],
+                    Text(
+                      'Vehicle Type: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
                     ),
                     Text(
-                      'two-wheels',
+                      vehicleType,
                       style: TextStyle(
-                        color: Colors.grey,
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
                       ),
                     ),
                   ],
                 ),
                 const SizedBox(height: 16),
-                const Text(
-                  'Carrying or loading the car of any material when its edge portion causes damage or scrape the pavement of the road/street.',
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Description: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      violation,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Status: ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      status,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                    Text(
+                      timestamp,
+                      style: TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.black,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Text(
+                  violation,
                   style: TextStyle(
                     color: Colors.black,
                   ),
@@ -186,36 +363,63 @@ class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
                   ),
                 ),
                 const SizedBox(height: 8),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Container(
-                      height: 160,
-                      width: 215,
-                      decoration: BoxDecoration(
-                        color: blueColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(10),
+                if (attachmentUrl1.isNotEmpty)
+                  Container(
+                    height: 160,
+                    width: 215,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all(
+                        color: Colors.grey.withOpacity(0.5),
                       ),
                     ),
-                    Container(
-                      height: 160,
-                      width: 215,
-                      decoration: BoxDecoration(
-                        color: blueColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(10),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        attachmentUrl1,
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    Container(
-                      height: 160,
-                      width: 215,
-                      decoration: BoxDecoration(
-                        color: blueColor.withOpacity(0.1),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
+                  ),
+                Container(
+                  height: 160,
+                  width: 215,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
                     ),
-                  ],
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      attachmentUrl2,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
                 ),
+                Container(
+                  height: 160,
+                  width: 215,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: Image.network(
+                      attachmentUrl3,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                // else
+                //   Text(
+                //     'No attachment available.',
+                //     style: TextStyle(color: Colors.grey),
+                //   ),
               ],
             ),
           ),
@@ -232,27 +436,18 @@ class _TicketsDesktopScreenState extends State<TicketsDesktopScreen> {
                 ),
               ),
             ),
-            TextButton(
-              style: TextButton.styleFrom(
-                backgroundColor: blueColor,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
-              ),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text(
-                "Resolve",
-                style: TextStyle(
-                  color: whiteColor,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),
           ],
         );
       },
     );
   }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => tickets.length;
+
+  @override
+  int get selectedRowCount => 0;
 }
