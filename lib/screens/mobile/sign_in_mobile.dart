afe6de9c16c3de909e5a0b579860a7bada8df305
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-10-01 21:38:52 +0800

    Screens: Update Backend Sign In Mobile

--- lib/screens/mobile/sign_in_mobile.dart
+++ lib/screens/mobile/sign_in_mobile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:park_in_web/components/fields/form_field.dart';
+import 'package:park_in_web/components/snackbar/error_snackbar.dart';
+import 'package:park_in_web/components/snackbar/success_snackbar.dart';
 import 'package:park_in_web/components/theme/color_scheme.dart';
 import 'package:park_in_web/components/ui/primary_btn.dart';
+import 'package:park_in_web/services/Auth/Auth_Service.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SignInMobileScreen extends StatefulWidget {
   const SignInMobileScreen({super.key});
@@ -13,6 +17,45 @@ class SignInMobileScreen extends StatefulWidget {
 class _SignInMobileScreenState extends State<SignInMobileScreen> {
   final TextEditingController _emailCtrl = TextEditingController();
   final TextEditingController _passwordCtrl = TextEditingController();
+
+  void login(BuildContext context) async {
+    final authService = AuthService();
+
+    if (_emailCtrl.text.isEmpty || _passwordCtrl.text.isEmpty) {
+      errorSnackbar(context, "Please fill out all fields.");
+      return;
+    }
+
+    try {
+      await authService.signInWithEmailPassword(
+        _emailCtrl.text,
+        _passwordCtrl.text,
+      );
+
+      final prefs = await SharedPreferences.getInstance();
+      await prefs.setString('userType', 'Admin'); // Store user type
+      await prefs.setBool('isLoggedIn', true); // Store login status
+
+      successSnackbar(context, "Sign in successful!");
+
+      Navigator.pushNamedAndRemoveUntil(
+        context,
+        '/reports',
+        (Route<dynamic> route) => false,
+      );
+    } catch (e) {
+      if (mounted) {
+        String errorMessage;
+        if (e is AuthServiceException) {
+          errorMessage = e.message;
+        } else {
+          errorMessage = 'An unknown error occurred. Try again later.';
+        }
+
+        errorSnackbar(context, errorMessage);
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -96,14 +139,17 @@ class _SignInMobileScreenState extends State<SignInMobileScreen> {
                         ),
                         PRKFormField(
                           prefixIcon: Icons.password_rounded,
+                          suffixIcon: Icons.visibility_off_rounded,
                           labelText: "Password",
                           controller: _passwordCtrl,
+                          obscureText: true,
                         ),
                         const Spacer(),
                         PRKPrimaryBtn(
                           label: "Sign In",
                           onPressed: () {
-                            Navigator.pushNamed(context, '/reports');
+                            login(context);
+                            // Navigator.pushNamed(context, '/reports');
                           },
                         ),
                       ],
