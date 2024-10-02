11329eb6a0136a3b38dbae3dd323eba8cf588b02
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-10-02 17:04:37 +0800

    Services: Update Auth Gate

--- lib/services/Auth/Auth_Gate.dart
+++ lib/services/Auth/Auth_Gate.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:park_in_web/screens/report_main.dart';
 import 'package:park_in_web/screens/sign_in_main.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthGate extends StatelessWidget {
   const AuthGate({super.key});
