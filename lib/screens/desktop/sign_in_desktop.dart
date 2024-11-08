2df2408b069b4f3ea1cb48c789c24cf71a62f6cb
MNJDen <124029755+MNJDen@users.noreply.github.com>
2024-11-08 18:17:36 +0800

    Components: Update Sign In Desktop

--- lib/screens/desktop/sign_in_desktop.dart
+++ lib/screens/desktop/sign_in_desktop.dart
@@ -4,7 +4,6 @@ import 'package:park_in_web/components/snackbar/error_snackbar.dart';
 import 'package:park_in_web/components/snackbar/success_snackbar.dart';
 import 'package:park_in_web/components/theme/color_scheme.dart';
 import 'package:park_in_web/components/ui/primary_btn.dart';
-// import 'package:park_in_web/screens/report_main.dart';
 import 'package:park_in_web/services/Auth/Auth_Service.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -23,7 +22,11 @@ class _SignInDesktopScreenState extends State<SignInDesktopScreen> {
     final authService = AuthService();
 
     if (_emailCtrl.text.isEmpty || _passwordCtrl.text.isEmpty) {
-      errorSnackbar(context, "Please fill out all fields.");
+      errorSnackbar(
+        context,
+        "Please fill out all fields.",
+        MediaQuery.of(context).size.width * 0.3,
+      );
       return;
     }
 
@@ -37,11 +40,15 @@ class _SignInDesktopScreenState extends State<SignInDesktopScreen> {
       await prefs.setString('userType', 'Admin'); // Store user type
       await prefs.setBool('isLoggedIn', true); // Store login status
 
-      successSnackbar(context, "Sign in successful!");
+      successSnackbar(
+        context,
+        "Sign in successful!",
+        MediaQuery.of(context).size.width * 0.3,
+      );
 
       Navigator.pushNamedAndRemoveUntil(
         context,
-        '/reports',
+        '/dashboard',
         (Route<dynamic> route) => false,
       );
     } catch (e) {
@@ -53,7 +60,11 @@ class _SignInDesktopScreenState extends State<SignInDesktopScreen> {
           errorMessage = 'An unknown error occurred. Try again later.';
         }
 
-        errorSnackbar(context, errorMessage);
+        errorSnackbar(
+          context,
+          errorMessage,
+          MediaQuery.of(context).size.width * 0.3,
+        );
       }
     }
   }
@@ -68,99 +79,173 @@ class _SignInDesktopScreenState extends State<SignInDesktopScreen> {
             child: Stack(
               fit: StackFit.expand,
               children: [
-                Positioned(
-                  top: -444,
-                  right: -26,
-                  child: Image.asset(
-                    'assets/images/top.png',
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Positioned(
-                  bottom: -581,
-                  left: -570,
-                  child: Image.asset(
-                    'assets/images/bot.png',
-                    fit: BoxFit.fill,
-                  ),
-                ),
-                Center(
-                  child: SizedBox(
-                    width: 450,
-                    height: 500,
-                    child: AspectRatio(
-                      aspectRatio: 4 / 3,
-                      child: Container(
-                        padding: const EdgeInsets.all(30),
-                        decoration: BoxDecoration(
-                          color: whiteColor,
-                          borderRadius: BorderRadius.circular(10),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black.withOpacity(0.1),
-                              blurRadius: 8,
-                              offset: const Offset(0, 4),
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Center(
-                              child: Image.asset(
-                                "assets/images/Logo.png",
-                                width: 40,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 55,
-                            ),
-                            const Text(
-                              "Sign In",
-                              style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.w600,
-                              ),
-                            ),
-                            const Text(
-                              "Enter your given credentials",
-                              style: TextStyle(
-                                fontSize: 12,
-                                fontWeight: FontWeight.w400,
-                              ),
-                            ),
-                            const SizedBox(
-                              height: 28,
-                            ),
-                            PRKFormField(
-                              prefixIcon: Icons.email_rounded,
-                              labelText: "Email Address",
-                              controller: _emailCtrl,
-                            ),
-                            const SizedBox(
-                              height: 16,
-                            ),
-                            PRKFormField(
-                              prefixIcon: Icons.password_rounded,
-                              suffixIcon: Icons.visibility_off_rounded,
-                              labelText: "Password",
-                              controller: _passwordCtrl,
-                              obscureText: true,
-                            ),
-                            const Spacer(),
-                            PRKPrimaryBtn(
-                              label: "Sign In",
-                              onPressed: () {
-                                login(context);
-                                // Navigator.pushNamed(context, '/reports');
-                              },
-                            ),
-                          ],
-                        ),
-                      ),
+                Container(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        blueColor,
+                        Color.fromRGBO(27, 29, 148, 1),
+                      ],
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
                     ),
                   ),
-                )
+                ),
+                Positioned(
+                  child: Opacity(
+                    opacity: 0.1,
+                    child: Image.asset(
+                      'assets/images/noise.png',
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: -41,
+                  left: -180,
+                  right: 0,
+                  child: Container(
+                    width: 331,
+                    height: 331,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: -281,
+                  left: 0,
+                  right: -30,
+                  child: Container(
+                    width: 823,
+                    height: 823,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 40,
+                  left: -50,
+                  child: Container(
+                    width: 260,
+                    height: 260,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.white.withOpacity(0.1),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  bottom: -259,
+                  left: -561,
+                  child: Image.asset(
+                    'assets/images/4-pillars.png',
+                    width: 1757,
+                    height: 1318,
+                  ),
+                ),
+                Positioned(
+                  top: 10,
+                  left: 10,
+                  child: Image.asset(
+                    'assets/images/s1.png',
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  // right: 0,
+                  left: 700,
+                  child: Image.asset(
+                    'assets/images/s2.png',
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(
+                      left: MediaQuery.of(context).size.width * .5),
+                  padding: EdgeInsets.symmetric(
+                    vertical: MediaQuery.of(context).size.height * .1,
+                    horizontal: MediaQuery.of(context).size.width * .13,
+                  ),
+                  decoration: const BoxDecoration(
+                    color: whiteColor,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          "assets/images/Logo.png",
+                          width: 35,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 120,
+                      ),
+                      const Text(
+                        "Sign In",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Text(
+                        "Enter your given credentials",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 28,
+                      ),
+                      PRKFormField(
+                        prefixIcon: Icons.alternate_email_rounded,
+                        labelText: "Email Address",
+                        controller: _emailCtrl,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      PRKFormField(
+                        prefixIcon: Icons.password_rounded,
+                        suffixIcon: Icons.visibility_off_rounded,
+                        labelText: "Password",
+                        controller: _passwordCtrl,
+                        obscureText: true,
+                      ),
+                      const Spacer(),
+                      PRKPrimaryBtn(
+                        label: "Sign In",
+                        onPressed: () {
+                          login(context);
+                          // Navigator.pushNamed(context, '/reports');
+                        },
+                      ),
+                      const SizedBox(
+                        height: 140,
+                      ),
+                      const Center(
+                        child: Text(
+                          "© 2024 Park-in. All Rights Reserved.",
+                          style: TextStyle(
+                            fontSize: 12,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  right: -180,
+                  bottom: -100,
+                  child: Image.asset(
+                    'assets/images/s3.png',
+                  ),
+                ),
               ],
             ),
           ),
