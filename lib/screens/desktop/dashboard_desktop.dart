0ac57cc06a48e1b33a8ade50e7e2f523877c8036
_clancybytes <90034280+DominicAEsperida@users.noreply.github.com>
2024-12-08 14:25:05 +0800

    Screens: Add Backend to Dashboard Desktop

--- lib/screens/desktop/dashboard_desktop.dart
+++ lib/screens/desktop/dashboard_desktop.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:park_in_web/components/navbar/navbar_desktop.dart';
 import 'package:park_in_web/components/theme/color_scheme.dart';
@@ -13,6 +15,33 @@ class DashboardDesktopScreen extends StatefulWidget {
 }
 
 class _DashboardDesktopScreenState extends State<DashboardDesktopScreen> {
+  final DatabaseReference _database =
+      FirebaseDatabase.instance.ref("parkingAreas");
+  final CollectionReference _violationTickets =
+      FirebaseFirestore.instance.collection("Violation Ticket");
+  final CollectionReference _incidentReports =
+      FirebaseFirestore.instance.collection("Incident Report");
+
+  Map<String, int> _calculateCounts(Map<dynamic, dynamic> data) {
+    int tempTwoWheelsCount = 0;
+    int tempFourWheelsCount = 0;
+
+    data.forEach((key, value) {
+      final int count = (value['count'] ?? 0) as int;
+
+      if (key == "Dolan (M)" || key == "Library (M)" || key == "Alingal (M)") {
+        tempTwoWheelsCount += count;
+      } else {
+        tempFourWheelsCount += count;
+      }
+    });
+
+    return {
+      'twoWheelsCount': tempTwoWheelsCount,
+      'fourWheelsCount': tempFourWheelsCount,
+    };
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,68 +54,148 @@ class _DashboardDesktopScreenState extends State<DashboardDesktopScreen> {
             Expanded(
               child: Container(
                 height: MediaQuery.of(context).size.height,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 20,
-                ),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                 decoration: BoxDecoration(
                   color: whiteColor,
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Column(
                   children: [
-                    Row(
-                      children: [
-                        PRKSmallCard(
-                          label: "Two-Wheels",
-                          content: "48",
-                          sub: "Parking spaces available",
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          color: parkingGreenColor,
-                          icon: Icons.two_wheeler_rounded,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.009,
-                        ),
-                        PRKSmallCard(
-                          label: "Four-Wheels",
-                          content: "36",
-                          sub: "c",
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          color: parkingYellowColor,
-                          icon: Icons.airport_shuttle_rounded,
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.009,
-                        ),
-                        PRKSmallCard(
-                          label: "Violations",
-                          content: "335",
-                          sub: "Infractions committed",
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          onPressed: () {},
-                        ),
-                        SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.009,
-                        ),
-                        PRKSmallCard(
-                          label: "Reports",
-                          content: "128",
-                          sub: "Received reports",
-                          height: MediaQuery.of(context).size.height * 0.2,
-                          onPressed: () {},
-                        ),
-                      ],
+                    StreamBuilder(
+                      stream: _database.onValue,
+                      builder:
+                          (context, AsyncSnapshot<DatabaseEvent> snapshot) {
+                        if (snapshot.hasData &&
+                            snapshot.data!.snapshot.value != null) {
+                          final data = snapshot.data!.snapshot.value
+                              as Map<dynamic, dynamic>;
+                          final counts = _calculateCounts(data);
+                          return Row(
+                            children: [
+                              PRKSmallCard(
+                                label: "Two-Wheels",
+                                content: "${counts['twoWheelsCount']}",
+                                sub: "Parking spaces available",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                color: parkingGreenColor,
+                                icon: Icons.two_wheeler_rounded,
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              PRKSmallCard(
+                                label: "Four-Wheels",
+                                content: "${counts['fourWheelsCount']}",
+                                sub: "Parking spaces available",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                color: parkingYellowColor,
+                                icon: Icons.airport_shuttle_rounded,
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              StreamBuilder<QuerySnapshot>(
+                                stream: _violationTickets.snapshots(),
+                                builder: (context,
+                                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                                  int violationCount = 0;
+                                  if (snapshot.hasData) {
+                                    violationCount = snapshot.data!.size;
+                                  }
+                                  return PRKSmallCard(
+                                    label: "Violations",
+                                    content: "$violationCount",
+                                    sub: "Infractions committed",
+                                    height: MediaQuery.of(context).size.height *
+                                        0.2,
+                                    onPressed: () {},
+                                  );
+                                },
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              StreamBuilder<QuerySnapshot>(
+                                stream: _incidentReports.snapshots(),
+                                builder: (context,
+                                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                                  int reportCount = 0;
+                                  if (snapshot.hasData) {
+                                    reportCount = snapshot.data!.size;
+                                  }
+                                  return PRKSmallCard(
+                                    label: "Reports",
+                                    content: "$reportCount",
+                                    sub: "Received Reports",
+                                    height: MediaQuery.of(context).size.height *
+                                        0.2,
+                                    onPressed: () {},
+                                  );
+                                },
+                              ),
+                            ],
+                          );
+                        } else {
+                          return Row(
+                            children: [
+                              PRKSmallCard(
+                                label: "Two-Wheels",
+                                content: "Loading...",
+                                sub: "Parking spaces available",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                color: parkingGreenColor,
+                                icon: Icons.two_wheeler_rounded,
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              PRKSmallCard(
+                                label: "Four-Wheels",
+                                content: "Loading...",
+                                sub: "Parking spaces available",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                color: parkingYellowColor,
+                                icon: Icons.airport_shuttle_rounded,
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              PRKSmallCard(
+                                label: "Violations",
+                                content: "Loading...",
+                                sub: "Infractions committed",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                onPressed: () {},
+                              ),
+                              SizedBox(
+                                  width: MediaQuery.of(context).size.width *
+                                      0.009),
+                              PRKSmallCard(
+                                label: "Reports",
+                                content: "Loading...",
+                                sub: "Received Reports",
+                                height:
+                                    MediaQuery.of(context).size.height * 0.2,
+                                onPressed: () {},
+                              ),
+                            ],
+                          );
+                        }
+                      },
                     ),
                     SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.015,
-                    ),
+                        height: MediaQuery.of(context).size.height * 0.015),
                     Row(
                       children: [
                         const PRKUserCard(),
                         SizedBox(
-                          width: MediaQuery.of(context).size.width * 0.009,
-                        ),
+                            width: MediaQuery.of(context).size.width * 0.009),
                         Expanded(
                           child: Container(
                             height: MediaQuery.of(context).size.height * 0.33,
@@ -95,9 +204,8 @@ class _DashboardDesktopScreenState extends State<DashboardDesktopScreen> {
                               color: whiteColor,
                               borderRadius: BorderRadius.circular(10),
                               border: Border.all(
-                                color: blackColor.withOpacity(0.1),
-                                width: 0.5,
-                              ),
+                                  color: blackColor.withOpacity(0.1),
+                                  width: 0.5),
                               boxShadow: [
                                 BoxShadow(
                                   color: blackColor.withOpacity(0.05),
@@ -133,7 +241,7 @@ class _DashboardDesktopScreenState extends State<DashboardDesktopScreen> {
                                         Icons.access_time_filled_rounded,
                                         color: blackColor,
                                       ),
-                                    )
+                                    ),
                                   ],
                                 ),
                                 SizedBox(
@@ -147,15 +255,14 @@ class _DashboardDesktopScreenState extends State<DashboardDesktopScreen> {
                       ],
                     ),
                     SizedBox(
-                      height: MediaQuery.of(context).size.height * 0.015,
-                    ),
+                        height: MediaQuery.of(context).size.height * 0.015),
                     const Row(
                       children: [PRKLargeCard()],
                     )
                   ],
                 ),
               ),
-            )
+            ),
           ],
         ),
       ),
