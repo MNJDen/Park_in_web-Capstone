39f7898f8a3b80a6e6fb37b753e98d9b843a919f
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-06 13:39:52 +0800

    Components: Update Color Theme

--- lib/components/theme/color_scheme.dart
+++ lib/components/theme/color_scheme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 const blackColor = Color.fromRGBO(27, 27, 27, 1);
 const borderBlack = Color.fromRGBO(27, 27, 27, 10);
-const blueColor = Color.fromRGBO(45, 49, 250, 1);
+const blueColor = Color.fromRGBO(0, 94, 255, 1); 
 const bgColor = Color.fromRGBO(4, 5, 25, 1);
 const whiteColor = Color.fromRGBO(255, 255, 255, 1);
 const yellowColor = Color.fromRGBO(255, 219, 35, 1);
