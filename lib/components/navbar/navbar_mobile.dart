cfa858fa4b2f731d2d92fdde015d5bbaa7d6128d
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-06 13:38:47 +0800

    Components: Update Navbar Mobile

--- lib/components/navbar/navbar_mobile.dart
+++ lib/components/navbar/navbar_mobile.dart
@@ -3,39 +3,20 @@ import 'package:park_in_web/components/theme/color_scheme.dart';
 
 class NavbarMobile extends StatelessWidget {
   final VoidCallback onMenuPressed;
-  final String pageName;
 
   const NavbarMobile({
     super.key,
     required this.onMenuPressed,
-    required this.pageName,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      // mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        IconButton(
-          onPressed: onMenuPressed,
-          icon: const Icon(
-            Icons.menu_rounded,
-            color: whiteColor,
-          ),
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Text(
-          pageName,
-          style: const TextStyle(
-            color: whiteColor,
-            fontSize: 20,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ],
+    return IconButton(
+      onPressed: onMenuPressed,
+      icon: const Icon(
+        Icons.menu_rounded,
+        color: whiteColor,
+      ),
     );
   }
 }
