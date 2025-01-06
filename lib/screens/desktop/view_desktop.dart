9e8660207083f4f6bc355aa0f99896ab4f3320c3
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-06 13:42:25 +0800

    Screens: Update View Desktop

--- lib/screens/desktop/view_desktop.dart
+++ lib/screens/desktop/view_desktop.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:park_in_web/components/theme/color_scheme.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 
 class ViewDesktopScreen extends StatefulWidget {
   const ViewDesktopScreen({super.key});
@@ -13,11 +16,56 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
   final DatabaseReference _dbRef =
       FirebaseDatabase.instance.ref('parkingAreas');
   int totalParkingCount = 0;
+  bool _isLoading = true;
+
+  late Timer _timer;
+  DateTime now = DateTime.now();
+
+  final List<String> monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  String _getDayOfWeek(int weekday) {
+    switch (weekday) {
+      case 1:
+        return "Monday";
+      case 2:
+        return "Tuesday";
+      case 3:
+        return "Wednesday";
+      case 4:
+        return "Thursday";
+      case 5:
+        return "Friday";
+      case 6:
+        return "Saturday";
+      case 7:
+        return "Sunday";
+      default:
+        return "";
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     _listenToParkingUpdates();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      setState(() {
+        now = DateTime.now();
+      });
+    });
   }
 
   // Listen to real-time updates from Firebase
@@ -37,6 +85,7 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
         if (mounted) {
           setState(() {
             totalParkingCount = total;
+            _isLoading = false;
           });
         }
       } else {
@@ -47,11 +96,18 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
 
   @override
   void dispose() {
+    _timer.cancel();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
+    String formattedTime =
+        "${now.hour % 12 == 0 ? 12 : now.hour % 12}:${now.minute.toString().padLeft(2, '0')}:${now.second.toString().padLeft(2, '0')} ${now.hour >= 12 ? 'PM' : 'AM'}";
+
+    String formattedDate =
+        "${monthNames[now.month - 1]} ${now.day.toString().padLeft(2, '0')}, ${now.year} | ${_getDayOfWeek(now.weekday)}";
+
     return Scaffold(
       backgroundColor: whiteColor,
       body: Column(
@@ -84,7 +140,7 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
                   ),
                 ),
                 Positioned(
-                  top: 80,
+                  bottom: -10,
                   right: -51,
                   child: Image.asset(
                     'assets/images/line1.png',
@@ -108,35 +164,45 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
                   left: 0,
                   right: 0,
                   bottom: 50,
-                  child: Column(
-                    children: [
-                      Container(
-                        height: 100,
-                        width: 100,
-                        decoration: BoxDecoration(
-                          color: blueColor.withOpacity(0.1),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Wrap(
+                      direction: Axis.vertical,
+                      crossAxisAlignment: WrapCrossAlignment.center,
+                      children: [
+                        Text(
+                          formattedTime,
+                          style: const TextStyle(
+                            color: blackColor,
+                            fontSize: 52,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      const Text(
-                        "Download the app now!",
-                        style: TextStyle(
-                          fontSize: 12,
-                          fontWeight: FontWeight.w400,
+                        Text(
+                          formattedDate,
+                          style: const TextStyle(
+                            color: blackColor,
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ).animate().fade(delay: const Duration(milliseconds: 200)),
                   ),
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 50),
+                Positioned(
+                  left: 0,
+                  right: 0,
+                  top: 50,
                   child: Align(
                     alignment: Alignment.topCenter,
                     child: Image.asset(
                       "assets/images/Logo.png",
-                      width: 40,
+                      width: 35,
+                      color: blueColor,
                     ),
                   ),
-                ),
+                ).animate().fade(delay: const Duration(milliseconds: 200)),
                 Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
@@ -147,31 +213,47 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
                         fontSize: 20,
                         fontWeight: FontWeight.w500,
                       ),
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
+                    ).animate().fade(delay: const Duration(milliseconds: 150)),
+                    // const SizedBox(
+                    //   height: 10,
+                    // ),
                     Center(
-                      child: TweenAnimationBuilder<int>(
-                        tween: IntTween(
-                          begin: 0,
-                          end: totalParkingCount,
-                        ),
-                        duration: const Duration(seconds: 1),
-                        builder: (context, value, child) {
-                          return Text(
-                            value.toString(),
-                            style: const TextStyle(
-                              fontSize: 200,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          );
-                        },
-                      ),
+                      child: _isLoading
+                          ? Container(
+                              margin: const EdgeInsets.symmetric(vertical: 75),
+                              // height: 150,
+                              child: LoadingAnimationWidget.waveDots(
+                                color: blueColor,
+                                size: 150,
+                              ),
+                            )
+                              .animate()
+                              .fade(delay: const Duration(milliseconds: 100))
+                          : TweenAnimationBuilder<int>(
+                              tween: IntTween(
+                                begin: 0,
+                                end: totalParkingCount,
+                              ),
+                              duration: const Duration(seconds: 1),
+                              builder: (context, value, child) {
+                                return Text(
+                                  value == 0 ? 'FULL' : value.toString(),
+                                  style: TextStyle(
+                                    fontSize: 200,
+                                    fontWeight: FontWeight.bold,
+                                    color: value == 0
+                                        ? parkingRedColor
+                                        : blackColor,
+                                  ),
+                                );
+                              },
+                            )
+                              .animate()
+                              .fade(delay: const Duration(milliseconds: 100)),
                     ),
-                    const SizedBox(
-                      height: 20,
-                    ),
+                    // const SizedBox(
+                    //   height: 20,
+                    // ),
                     const Text(
                       "Approximately",
                       style: TextStyle(
@@ -179,9 +261,61 @@ class _ViewDesktopScreenState extends State<ViewDesktopScreen> {
                         fontWeight: FontWeight.w300,
                         fontStyle: FontStyle.italic,
                       ),
-                    ),
+                    ).animate().fade(delay: const Duration(milliseconds: 150)),
                   ],
                 ),
+                // Positioned(
+                //   right: 10,
+                //   bottom: 10,
+                //   child: Column(
+                //     children: [
+                //       Container(
+                //         padding: const EdgeInsets.all(20),
+                //         decoration: BoxDecoration(
+                //           color: whiteColor,
+                //           borderRadius: BorderRadius.circular(10),
+                //           border: Border.all(
+                //               color: blackColor.withOpacity(0.1), width: 0.5),
+                //           boxShadow: [
+                //             BoxShadow(
+                //               color: blackColor.withOpacity(0.05),
+                //               blurRadius: 8,
+                //               offset: const Offset(0, 4),
+                //             ),
+                //           ],
+                //         ),
+                //         child: Image.asset(
+                //           "assets/images/QR.png",
+                //           height: 100,
+                //         ),
+                //       ),
+                //       Container(
+                //         padding: const EdgeInsets.all(20),
+                //         decoration: BoxDecoration(
+                //           color: blackColor,
+                //           borderRadius: BorderRadius.circular(10),
+                //           border: Border.all(
+                //               color: blackColor.withOpacity(0.1), width: 0.5),
+                //           boxShadow: [
+                //             BoxShadow(
+                //               color: blackColor.withOpacity(0.05),
+                //               blurRadius: 8,
+                //               offset: const Offset(0, 4),
+                //             ),
+                //           ],
+                //         ),
+                //         child: const Text(
+                //           "Download the app now!",
+                //           style: TextStyle(
+                //             fontSize: 12,
+                //             color: whiteColor,
+                //             fontWeight: FontWeight.w400,
+                //           ),
+                //         ),
+                //       ),
+                //     ],
+                //   ),
+                // ),
                 Positioned(
                   left: 10,
                   top: 10,
