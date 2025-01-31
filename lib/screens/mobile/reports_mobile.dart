21dcb902fa15ba022d0d6d3fd863a3a2daf42429
MNJDen <124029755+MNJDen@users.noreply.github.com>
2025-01-31 19:58:00 +0800

    Screens: Update Reports Mobile

--- lib/screens/mobile/reports_mobile.dart
+++ lib/screens/mobile/reports_mobile.dart
@@ -489,6 +489,21 @@ class _ReportsMobileScreenState extends State<ReportsMobileScreen> {
                   ),
                   ListTile(
                     leading: const Icon(
+                      Icons.people_rounded,
+                      color: whiteColor,
+                    ),
+                    title: const Text(
+                      'Users',
+                      style: TextStyle(
+                        color: whiteColor,
+                      ),
+                    ),
+                    onTap: () {
+                      _onItemTap('Users');
+                    },
+                  ),
+                  ListTile(
+                    leading: const Icon(
                       Icons.flag_outlined,
                       color: whiteColor,
                     ),
